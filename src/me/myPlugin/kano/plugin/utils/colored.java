1281fd279601cf301fc6028128ef26c538408a80
Kyanoe <73148419+Kyanoe@users.noreply.github.com>
2021-02-13 16:27:59 +0700

    Add files via upload

---
+++ src/me/myPlugin/kano/plugin/utils/colored.java
@@ -0,0 +1,10 @@
+package me.myPlugin.kano.plugin.utils;
+
+import org.bukkit.ChatColor;
+
+public class colored {
+
+    public static String colored(String text){
+        return ChatColor.translateAlternateColorCodes('&',text);
+    }
+}
